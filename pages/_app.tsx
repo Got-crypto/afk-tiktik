f78d7ec1748cc903581de9d8a8c4e82e479de5ae
Got-crypto <pecpalmplay@gmail.com>
2023-01-07 23:44:41 +0200

    designing a sidebar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useState, useEffect } from 'react'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import * as dotenv from 'dotenv'
 
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
+
+dotenv.config()
 
 export default function App({ Component, pageProps }: AppProps) {
   
@@ -15,17 +19,23 @@ export default function App({ Component, pageProps }: AppProps) {
   
   if(isSSR) return null
 
+
+  
+  console.log('process.env.REACT_PUBLIC_GOOGLE_API_KEY', process.env.REACT_PUBLIC_GOOGLE_API_KEY)
+
   return (
-    <div>
-      <Navbar />
-      <div className='flex gap-6 md:gap-20'>
-        <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto '>
-          <Sidebar />
-        </div>
-        <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
-          <Component {...pageProps} />
+    <GoogleOAuthProvider clientId='{process.env.REACT_PUBLIC_GOOGLE_API_KEY}'>
+      <div>
+        <Navbar />
+        <div className='flex gap-6 md:gap-20'>
+          <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto '>
+            <Sidebar />
+          </div>
+          <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
+            <Component {...pageProps} />
+          </div>
         </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   )
 }
